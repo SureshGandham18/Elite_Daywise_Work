77e42dc98e4efdff753e589f7cd807a3bb82805a
Shanmukha Naga Suresh Gandham <85335126+SureshGandham18@users.noreply.github.com>
2023-02-15 16:16:55 +0530

    Update courseschedule2.java

--- 09-02-23/courseschedule2.java
+++ 09-02-23/courseschedule2.java
@@ -1,47 +1,38 @@
 class Solution {
-  public int[] findOrder(int numCourses, int[][] prerequisites) {
+    ArrayList<ArrayList<Integer>> adj;
+    int[] ind;
+    Queue<Integer> q;
+    boolean[] vis;
+    ArrayList<Integer> res;
 
-    boolean isPossible = true;
-    Map<Integer, List<Integer>> adjList = new HashMap<Integer, List<Integer>>();
-    int[] indegree = new int[numCourses];
-    int[] topologicalOrder = new int[numCourses];
-
-    for (int i = 0; i < prerequisites.length; i++) {
-      int dest = prerequisites[i][0];
-      int src = prerequisites[i][1];
-      List<Integer> lst = adjList.getOrDefault(src, new ArrayList<Integer>());
-      lst.add(dest);
-      adjList.put(src, lst);
-
-     
-      indegree[dest] += 1;
-    }
-    Queue<Integer> q = new LinkedList<Integer>();
-    for (int i = 0; i < numCourses; i++) {
-      if (indegree[i] == 0) {
-        q.add(i);
-      }
-    }
-
-    int i = 0;
-    while (!q.isEmpty()) {
-      int node = q.remove();
-      topologicalOrder[i++] = node;
-      if (adjList.containsKey(node)) {
-        for (Integer neighbor : adjList.get(node)) {
-          indegree[neighbor]--;
-          if (indegree[neighbor] == 0) {
-            q.add(neighbor);
-          }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        adj = new ArrayList<>();
+        ind = new int[numCourses];
+        q = new LinkedList<>();
+        vis = new boolean[numCourses];
+        res = new ArrayList<>();
+        for(int i=0;i<numCourses;i++) adj.add(new ArrayList<Integer>());
+        for(int i=0;i<prerequisites.length;i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            ind[prerequisites[i][0]]++;
         }
-      }
+        for(int i=0;i<ind.length;i++){
+            if(ind[i]==0) q.offer(i);
+        }
+        while(!q.isEmpty()){
+            int k = q.poll();
+            vis[k] = true;
+            res.add(k);
+            for(int nei:adj.get(k)){
+                if(!vis[nei]){
+                    ind[nei]--;
+                    if(ind[nei]==0) q.offer(nei);
+                }
+            }
+        }
+        int[] fres = new int[res.size()];
+        for(int i=0;i<res.size();i++) fres[i] = res.get(i);
+        if(fres.length==numCourses) return fres;
+        return new int[]{};
     }
-
-  
-    if (i == numCourses) {
-      return topologicalOrder;
-    }
-
-    return new int[0];
-  }
 }
